4d82e97be721f857550324420ac20257e61445c9
Edson Bueno <ejr.bueno@gmail.com>
2020-09-30 08:39:54 -0300

    Add state properties' aliases to PagingController.

--- lib/src/workers/paging_controller.dart
+++ lib/src/workers/paging_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/widgets.dart';
+import 'package:infinite_scroll_pagination/src/core/paging_state.dart';
 import 'package:infinite_scroll_pagination/src/core/paging_status.dart';
 
 typedef PageRequestListener<PageKeyType> = void Function(
@@ -8,12 +10,36 @@ typedef PagingStatusListener = void Function(
   PagingStatus status,
 );
 
-abstract class PagingController<PageKeyType, ItemType> {
-  void addPageRequestListener(PageRequestListener<PageKeyType> listener);
-  void removePageRequestListener(PageRequestListener<PageKeyType> listener);
-  void notifyPageRequestListeners();
+class PagingController<PageKeyType, ItemType>
+    extends ValueNotifier<PagingState<PageKeyType, ItemType>> {
+  PagingController(PageKeyType firstPageKey)
+      : super(
+          PagingState<PageKeyType, ItemType>(nextPageKey: firstPageKey),
+        );
 
-  void addPagingStatusListener(PagingStatusListener listener);
-  void removePagingStatusListener(PagingStatusListener listener);
-  void notifyPagingStatusListeners();
+  List<ItemType> get itemList => value.itemList;
+
+  dynamic get error => value.error;
+
+  PageKeyType get nextPageKey => value.nextPageKey;
+
+  void addNewPage(List<ItemType> newItems, PageKeyType nextPageKey) {
+    final previousItems = value.itemList ?? [];
+    final itemList = previousItems + newItems;
+    value = value.copyWith(
+      itemList: itemList,
+      error: null,
+      nextPageKey: nextPageKey,
+    );
+  }
+
+  void addLastPage(List<ItemType> newItems) => addNewPage(newItems, null);
+
+  void addPageRequestListener(PageRequestListener<PageKeyType> listener) {}
+  void removePageRequestListener(PageRequestListener<PageKeyType> listener) {}
+  void notifyPageRequestListeners() {}
+
+  void addPagingStatusListener(PagingStatusListener listener) {}
+  void removePagingStatusListener(PagingStatusListener listener) {}
+  void notifyPagingStatusListeners() {}
 }
