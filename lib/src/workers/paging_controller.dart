a570b881b0309638ff5bf5dd944f635639db5ea7
Edson Bueno <ejr.bueno@gmail.com>
2020-10-01 05:23:13 -0300

    Add Status and PageRequest listeners.

--- lib/src/workers/paging_controller.dart
+++ lib/src/workers/paging_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:infinite_scroll_pagination/src/core/paging_state.dart';
 import 'package:infinite_scroll_pagination/src/core/paging_status.dart';
@@ -12,14 +13,37 @@ typedef PagingStatusListener = void Function(
 
 class PagingController<PageKeyType, ItemType>
     extends ValueNotifier<PagingState<PageKeyType, ItemType>> {
-  PagingController(PageKeyType firstPageKey)
-      : super(
+  PagingController({
+    @required this.firstPageKey,
+    this.invisibleItemsThreshold,
+  })  : assert(
+          firstPageKey != null,
+        ),
+        super(
           PagingState<PageKeyType, ItemType>(nextPageKey: firstPageKey),
         );
+
+  /// The number of items before the end of the list that triggers a new fetch.
+  final int invisibleItemsThreshold;
+
+  /// The key for the first page to be fetched.
+  ///
+  /// Needed for being able to reset state.
+  final PageKeyType firstPageKey;
+
+  ObserverList<PagingStatusListener> _statusListeners =
+      ObserverList<PagingStatusListener>();
+  ObserverList<PageRequestListener> _pageRequestListeners =
+      ObserverList<PageRequestListener>();
 
   List<ItemType> get itemList => value.itemList;
 
   dynamic get error => value.error;
+  set error(dynamic newError) {
+    value = value.copyWith(
+      error: newError,
+    );
+  }
 
   PageKeyType get nextPageKey => value.nextPageKey;
 
@@ -35,11 +59,63 @@ class PagingController<PageKeyType, ItemType>
 
   void addLastPage(List<ItemType> newItems) => addNewPage(newItems, null);
 
-  void addPageRequestListener(PageRequestListener<PageKeyType> listener) {}
-  void removePageRequestListener(PageRequestListener<PageKeyType> listener) {}
-  void notifyPageRequestListeners() {}
+  /// Erases the current error so that we're back to loading state and retries
+  /// the latest request.
+  void retryLastRequest() {
+    error = null;
+    notifyPageRequestListeners(nextPageKey);
+  }
 
-  void addPagingStatusListener(PagingStatusListener listener) {}
-  void removePagingStatusListener(PagingStatusListener listener) {}
-  void notifyPagingStatusListeners() {}
+  /// Resets `this` to its initial state and fetches the initial key again.
+  void reset() {
+    value = PagingState<PageKeyType, ItemType>(
+      nextPageKey: firstPageKey,
+      error: null,
+      itemList: null,
+    );
+
+    notifyPageRequestListeners(firstPageKey);
+  }
+
+  void addStatusListener(PagingStatusListener listener) {
+    _statusListeners.add(listener);
+  }
+
+  void removeStatusListener(PagingStatusListener listener) {
+    _statusListeners.remove(listener);
+  }
+
+  void notifyStatusListeners(PagingStatus status) {
+    final localListeners = List<PagingStatusListener>.from(_statusListeners);
+    for (final listener in localListeners) {
+      if (_statusListeners.contains(listener)) {
+        listener(status);
+      }
+    }
+  }
+
+  void addPageRequestListener(PageRequestListener listener) {
+    _pageRequestListeners.add(listener);
+  }
+
+  void removePageRequestListener(PageRequestListener listener) {
+    _pageRequestListeners.remove(listener);
+  }
+
+  void notifyPageRequestListeners(PageKeyType pageKey) {
+    final localListeners =
+        List<PageRequestListener>.from(_pageRequestListeners);
+    for (final listener in localListeners) {
+      if (_pageRequestListeners.contains(listener)) {
+        listener(pageKey);
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _statusListeners = null;
+    _pageRequestListeners = null;
+    super.dispose();
+  }
 }
