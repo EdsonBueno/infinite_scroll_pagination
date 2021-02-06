49f0ada2f76c6b795521d01551fac2d3713dfc65
Edson Bueno <ejr.bueno@gmail.com>
2021-02-06 07:36:38 -0300

    Migrate FirstPageExceptionIndicator.

--- lib/src/ui/default_indicators/first_page_exception_indicator.dart
+++ lib/src/ui/default_indicators/first_page_exception_indicator.dart
@@ -4,61 +4,65 @@ import 'package:flutter/material.dart';
 /// Basic layout for indicating that an exception occurred.
 class FirstPageExceptionIndicator extends StatelessWidget {
   const FirstPageExceptionIndicator({
-    @required this.title,
+    required this.title,
     this.message,
     this.onTryAgain,
-    Key key,
-  })  : assert(title != null),
-        super(key: key);
+    Key? key,
+  }) : super(key: key);
+
   final String title;
-  final String message;
-  final VoidCallback onTryAgain;
+  final String? message;
+  final VoidCallback? onTryAgain;
 
   @override
-  Widget build(BuildContext context) => Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-          child: Column(
-            children: [
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline6,
+  Widget build(BuildContext context) {
+    // Necessary for the Collection If to work with Null Safety.
+    final message = this.message;
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+        child: Column(
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            if (message != null)
+              const SizedBox(
+                height: 16,
               ),
-              if (message != null)
-                const SizedBox(
-                  height: 16,
-                ),
-              if (message != null)
-                Text(
-                  message,
-                  textAlign: TextAlign.center,
-                ),
-              if (onTryAgain != null)
-                const SizedBox(
-                  height: 48,
-                ),
-              if (onTryAgain != null)
-                SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: RaisedButton.icon(
-                    onPressed: onTryAgain,
-                    icon: const Icon(
-                      Icons.refresh,
+            if (message != null)
+              Text(
+                message,
+                textAlign: TextAlign.center,
+              ),
+            if (onTryAgain != null)
+              const SizedBox(
+                height: 48,
+              ),
+            if (onTryAgain != null)
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  onPressed: onTryAgain,
+                  icon: const Icon(
+                    Icons.refresh,
+                    color: Colors.white,
+                  ),
+                  label: const Text(
+                    'Try Again',
+                    style: TextStyle(
+                      fontSize: 16,
                       color: Colors.white,
-                    ),
-                    label: const Text(
-                      'Try Again',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.white,
-                      ),
                     ),
                   ),
                 ),
-            ],
-          ),
+              ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
