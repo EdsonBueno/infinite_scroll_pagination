96850e9bd5bfe063a2e6dfae7699a6919eaf4e50
clragon <clragon@clynamic.net>
2024-10-13 16:35:29 +0200

    refactor: move paging status to extension

--- lib/src/model/paging_state.dart
+++ lib/src/model/paging_state.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/foundation.dart';
-import 'package:infinite_scroll_pagination/src/model/paging_status.dart';
 
-/// The current item's list, error, and next page key state for a paginated
-/// widget.
+/// The current item's list, error, and next page key
+/// for a paginated widget.
 @immutable
 class PagingState<PageKeyType, ItemType> {
   const PagingState({
@@ -20,45 +19,17 @@ class PagingState<PageKeyType, ItemType> {
   /// The key for the next page to be fetched.
   final PageKeyType? nextPageKey;
 
-  /// The current pagination status.
-  PagingStatus get status {
-    if (_isOngoing) {
-      return PagingStatus.ongoing;
-    }
-
-    if (_isCompleted) {
-      return PagingStatus.completed;
-    }
-
-    if (_isLoadingFirstPage) {
-      return PagingStatus.loadingFirstPage;
-    }
-
-    if (_hasSubsequentPageError) {
-      return PagingStatus.subsequentPageError;
-    }
-
-    if (_isEmpty) {
-      return PagingStatus.noItemsFound;
-    } else {
-      return PagingStatus.firstPageError;
-    }
-  }
-
   @override
-  String toString() =>
-      '${objectRuntimeType(this, 'PagingState')}(itemList: \u2524'
-      '$itemList\u251C, error: $error, nextPageKey: $nextPageKey)';
+  String toString() => '${objectRuntimeType(this, 'PagingState')}'
+      '(itemList: \u2524$itemList\u251C, error: $error, nextPageKey: $nextPageKey)';
 
   @override
   bool operator ==(Object other) {
-    if (identical(this, other)) {
-      return true;
-    }
-    return other is PagingState &&
-        other.itemList == itemList &&
-        other.error == error &&
-        other.nextPageKey == nextPageKey;
+    return identical(this, other) ||
+        (other is PagingState &&
+            other.itemList == itemList &&
+            other.error == error &&
+            other.nextPageKey == nextPageKey);
   }
 
   @override
@@ -67,27 +38,4 @@ class PagingState<PageKeyType, ItemType> {
         error.hashCode,
         nextPageKey.hashCode,
       );
-
-  int? get _itemCount => itemList?.length;
-
-  bool get _hasNextPage => nextPageKey != null;
-
-  bool get _hasItems {
-    final itemCount = _itemCount;
-    return itemCount != null && itemCount > 0;
-  }
-
-  bool get _hasError => error != null;
-
-  bool get _isListingUnfinished => _hasItems && _hasNextPage;
-
-  bool get _isOngoing => _isListingUnfinished && !_hasError;
-
-  bool get _isCompleted => _hasItems && !_hasNextPage;
-
-  bool get _isLoadingFirstPage => _itemCount == null && !_hasError;
-
-  bool get _hasSubsequentPageError => _isListingUnfinished && _hasError;
-
-  bool get _isEmpty => _itemCount != null && _itemCount == 0;
 }
--- lib/src/model/paging_status.dart
+++ lib/src/model/paging_status.dart
@@ -1,3 +1,5 @@
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
 /// All possible status for a pagination.
 enum PagingStatus {
   completed,
@@ -6,4 +8,55 @@ enum PagingStatus {
   ongoing,
   firstPageError,
   subsequentPageError,
+}
+
+/// Extension methods for [PagingState] to determine the current status.
+extension PagingStatusExtension on PagingState {
+  int? get _itemCount => itemList?.length;
+
+  bool get _hasNextPage => nextPageKey != null;
+
+  bool get _hasItems {
+    final itemCount = _itemCount;
+    return itemCount != null && itemCount > 0;
+  }
+
+  bool get _hasError => error != null;
+
+  bool get _isListingUnfinished => _hasItems && _hasNextPage;
+
+  bool get _isOngoing => _isListingUnfinished && !_hasError;
+
+  bool get _isCompleted => _hasItems && !_hasNextPage;
+
+  bool get _isLoadingFirstPage => _itemCount == null && !_hasError;
+
+  bool get _hasSubsequentPageError => _isListingUnfinished && _hasError;
+
+  bool get _isEmpty => _itemCount != null && _itemCount == 0;
+
+  /// The current pagination status.
+  PagingStatus get status {
+    if (_isOngoing) {
+      return PagingStatus.ongoing;
+    }
+
+    if (_isCompleted) {
+      return PagingStatus.completed;
+    }
+
+    if (_isLoadingFirstPage) {
+      return PagingStatus.loadingFirstPage;
+    }
+
+    if (_hasSubsequentPageError) {
+      return PagingStatus.subsequentPageError;
+    }
+
+    if (_isEmpty) {
+      return PagingStatus.noItemsFound;
+    } else {
+      return PagingStatus.firstPageError;
+    }
+  }
 }
